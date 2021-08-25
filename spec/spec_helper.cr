9a263418e0769a87c007a87f57b2f0f60ed05d2a
HuyNguyenAu <huynguyenau@gmail.com>
2021-08-25 22:46:26 +1000

    Working scanner and passing chap04_scanning test.

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/lox-lang-crystal"
---
+++ src/main.cr
@@ -0,0 +1,57 @@
+require "../src/scanner.cr"
+
+class Program
+  @@had_error : Bool = false
+
+  def initialize()
+    if ARGC_UNSAFE > 2
+      puts "Usage: jlox [script]"
+      exit(64)
+    elsif ARGC_UNSAFE == 2
+      run_file(ARGF.gets_to_end)
+    else
+      run_prompt()
+    end
+  end
+  
+  def run_file(content : String)
+    run(content)
+    
+    if @@had_error
+      exit(65)
+    end
+  end
+  
+  def run(source : String)
+    scanner = Scanner.new(source)
+    tokens = scanner.scan_tokens()
+
+    tokens.each { |token| puts token.to_string }
+    # tokens.each { |token| p! token }
+    # puts tokens
+  end
+  
+  def run_prompt()
+    loop do
+      print "> "
+      
+      line : String | Nil = gets
+      if line.nil?
+        break
+      end
+      
+      run(line)
+    end
+  end
+
+  def self.error(line : Int32, message : String)
+    self.report(line, "", message)
+  end
+
+  def self.report(line : Int32, where : String, message : String)
+    puts "[line #{line}] Error#{where}: #{message}"
+    @@had_error = true
+  end
+end
+
+program = Program.new()
