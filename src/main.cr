5f401ce460d2fa043afe465121794ee5268b9d4f
HuyNguyenAu <huynguyenau@gmail.com>
2021-09-04 17:17:04 +1000

    Formatted and commented.

--- src/main.cr
+++ src/main.cr
@@ -3,7 +3,7 @@ require "../src/scanner.cr"
 class Program
   @@had_error : Bool = false
 
-  def initialize()
+  def initialize
     if ARGC_UNSAFE > 2
       puts "Usage: jlox [script]"
       exit(64)
@@ -13,43 +13,48 @@ class Program
       run_prompt()
     end
   end
-  
-  def run_file(content : String)
-    run(content)
-    
+
+  # Execute the provided source.
+  def run_file(source : String)
+    run(source)
+
     if @@had_error
       exit(65)
     end
   end
-  
+
+  # WIP
   def run(source : String)
     scanner : Scanner = Scanner.new(source)
-    tokens : Array(Token) = scanner.scan_tokens()
+    tokens : Array(Token) = scanner.scan_tokens
 
     tokens.each { |token| puts token.to_string }
   end
-  
-  def run_prompt()
+
+  # Run an interactive prompt.
+  def run_prompt
     loop do
       print "> "
-      
+
       line : String | Nil = gets
       if line.nil?
         break
       end
-      
+
       run(line)
     end
   end
 
+  # Print out the error and line number.
   def self.error(line : Int32, message : String)
     self.report(line, "", message)
   end
 
+  # Print out the error and line number.
   def self.report(line : Int32, where : String, message : String)
     puts "[line #{line}] Error#{where}: #{message}"
     @@had_error = true
   end
 end
 
-program = Program.new()
+program = Program.new
--- src/token.cr
+++ src/token.cr
@@ -1,41 +1,41 @@
 require "../src/token-type.cr"
 
 class Token
-    @type : TokenType = TokenType::NOVAL
-    @lexeme : String = ""
-    @literal : String | Float64
-    @line : Int32 = 0
-    @null : Bool = false # Used to pass tests since Crystal does not support nil like Java's null.
-                         # When you print Java's null it outputs "null", whereas Crystal outputs "".
+  @type : TokenType = TokenType::NOVAL
+  @lexeme : String = ""
+  @literal : String | Float64
+  @line : Int32 = 0
+  @null : Bool = false # Used to pass tests since Crystal does not support nil like Java's null.
+  # When you print Java's null it outputs "null", whereas Crystal outputs "".
 
-    # Okay, I'm not sure if this will be allowed in the future, but I don't see any fault
-    # with this. Since we can't use an Object instance variable, we can overload the
-    # intialisation of the class instead so that it can act like it can take Object params.
-    # But we need to know what specific types we are dealing with in the future.
+  # Okay, I'm not sure if this will be allowed in the future, but I don't see any fault
+  # with this. Since we can't use an Object instance variable, we can overload the
+  # intialisation of the class instead so that it can act like it can take Object params.
+  # But we need to know what specific types we are dealing with in the future.
 
-    def initialize(type : TokenType, lexeme : String, literal : String, line : Int32, null : Bool = false)
-        @type = type
-        @lexeme = lexeme
-        @literal = literal
-        @line = line
-        @null = null
+  def initialize(type : TokenType, lexeme : String, literal : String, line : Int32, null : Bool = false)
+    @type = type
+    @lexeme = lexeme
+    @literal = literal
+    @line = line
+    @null = null
+  end
+
+  def initialize(type : TokenType, lexeme : String, literal : Float64, line : Int32, null : Bool = false)
+    @type = type
+    @lexeme = lexeme
+    @literal = literal
+    @line = line
+    @null = null
+  end
+
+  # Used to show where a particular warning or error is.
+  def to_string : String
+    # Since Crystal handles nil differently to Java's null, in order to pass the provided tests,
+    # we need to modify the output so it behaves like the Java implementation.
+    if @null
+      @literal = "null"
     end
-
-    def initialize(type : TokenType, lexeme : String, literal : Float64, line : Int32, null : Bool = false)
-        @type = type
-        @lexeme = lexeme
-        @literal = literal
-        @line = line
-        @null = null
-    end
-
-    # Used to show where a particular warning or error is.
-    def to_string() : String
-        # Since Crystal handles nil differently to Java's null, in order to pass the provided tests,
-        # we need to modify the output so it behaves like the Java implementation.
-        if @null
-            @literal = "null"
-        end
-        "#{@type} #{@lexeme} #{@literal}"
-    end
+    "#{@type} #{@lexeme} #{@literal}"
+  end
 end
