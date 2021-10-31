a236baa8d3c0aed644c40d16ee89fb7a4cbd505c
HuyNguyenAu <huynguyenau@gmail.com>
2021-10-31 11:54:36 +0000

    Commented out unrequired visits.

--- src/expression.cr
+++ src/expression.cr
@@ -1,32 +1,40 @@
 require "../src/token.cr"
 
 abstract class Visitor
-  abstract def visit(expression : Assign)
+  # abstract def visit(expression : Assign)
   abstract def visit(expression : Binary)
-  abstract def visit(expression : Call)
-  abstract def visit(expression : Get)
-  abstract def visit(expression : Group)
+  # abstract def visit(expression : Call)
+  # abstract def visit(expression : Gets)
+  abstract def visit(expression : Grouping)
   abstract def visit(expression : Literal)
-  abstract def visit(expression : Logical)
-  abstract def visit(expression : Set)
-  abstract def visit(expression : Super)
-  abstract def visit(expression : This)
+  # abstract def visit(expression : Logical)
+  # abstract def visit(expression : Sets)
+  # abstract def visit(expression : Super)
+  # abstract def visit(expression : This)
   abstract def visit(expression : Unary)
-  abstract def visit(expression : Variable)
+  # abstract def visit(expression : Variable)
 end
 
 abstract class Expression
   abstract def accept(vistor : Visitor)
 end
 
-class Assign < Expression
-  def initialize(@token : Token, @value : Expression)
-  end
+# class Assign < Expression
+#   def initialize(@token : Token, @value : Expression)
+#   end
 
-  def accept(visitor : Visitor)
-    visitor.visit(self)
-  end
-end
+#   def accept(visitor : Visitor)
+#     visitor.visit(self)
+#   end
+
+#   def token
+#     @token
+#   end
+
+#   def value
+#     @value
+#   end
+# end
 
 class Binary < Expression
   def initialize(@left : Expression, @operator : Token, @right : Expression)
@@ -35,25 +43,57 @@ class Binary < Expression
   def accept(visitor : Visitor)
     visitor.visit(self)
   end
-end
 
-class Call < Expression
-  def initialize(@callee : Expression, @paren : Token, @arguments : Array(Expression))
+  def left
+    @left
   end
 
-  def accept(visitor : Visitor)
-    visitor.visit(self)
-  end
-end
-
-class Gets < Expression
-  def initialize(@object : Expression, @name : Token)
+  def operator
+    @operator
   end
 
-  def accept(visitor : Visitor)
-    visitor.visit(self)
+  def right
+    @right
   end
 end
+
+# class Call < Expression
+#   def initialize(@callee : Expression, @paren : Token, @arguments : Array(Expression))
+#   end
+
+#   def accept(visitor : Visitor)
+#     visitor.visit(self)
+#   end
+
+#   def callee
+#     @callee
+#   end
+
+#   def paren
+#     @paren
+#   end
+
+#   def arguments
+#     @arguments
+#   end
+# end
+
+# class Gets < Expression
+#   def initialize(@object : Expression, @name : Token)
+#   end
+
+#   def accept(visitor : Visitor)
+#     visitor.visit(self)
+#   end
+
+#   def object
+#     @object
+#   end
+
+#   def name
+#     @name
+#   end
+# end
 
 class Grouping < Expression
   def initialize(@expression : Expression)
@@ -61,6 +101,10 @@ class Grouping < Expression
 
   def accept(visitor : Visitor)
     visitor.visit(self)
+  end
+
+  def expression
+    @expression
   end
 end
 
@@ -71,43 +115,59 @@ class Literal < Expression
   def accept(visitor : Visitor)
     visitor.visit(self)
   end
-end
 
-class Logical < Expression
-  def initialize(@left : Expression, @operator : Token, @right : Expression)
-  end
-
-  def accept(visitor : Visitor)
-    visitor.visit(self)
+  def value
+    @value
   end
 end
 
-class Sets < Expression
-  def initialize(@object : Expression, @name : Token, @value : Expression)
-  end
+# class Logical < Expression
+#   def initialize(@left : Expression, @operator : Token, @right : Expression)
+#   end
 
-  def accept(visitor : Visitor)
-    visitor.visit(self)
-  end
-end
+#   def accept(visitor : Visitor)
+#     visitor.visit(self)
+#   end
 
-class Super < Expression
-  def initialize(@keyword : Token, @method : Token)
-  end
+#   def left
+#     @left
+#   end
 
-  def accept(visitor : Visitor)
-    visitor.visit(self)
-  end
-end
+#   def operator
+#     @operator
+#   end
 
-class This < Expression
-  def initialize(@keyword : Token)
-  end
+#   def right
+#     @right
+#   end
+# end
 
-  def accept(visitor : Visitor)
-    visitor.visit(self)
-  end
-end
+# class Sets < Expression
+#   def initialize(@object : Expression, @name : Token, @value : Expression)
+#   end
+
+#   def accept(visitor : Visitor)
+#     visitor.visit(self)
+#   end
+# end
+
+# class Super < Expression
+#   def initialize(@keyword : Token, @method : Token)
+#   end
+
+#   def accept(visitor : Visitor)
+#     visitor.visit(self)
+#   end
+# end
+
+# class This < Expression
+#   def initialize(@keyword : Token)
+#   end
+
+#   def accept(visitor : Visitor)
+#     visitor.visit(self)
+#   end
+# end
 
 class Unary < Expression
   def initialize(@operator : Token, @right : Expression)
@@ -116,13 +176,21 @@ class Unary < Expression
   def accept(visitor : Visitor)
     visitor.visit(self)
   end
-end
 
-class Variable < Expression
-  def initialize(@name : Token)
+  def operator
+    @operator
   end
 
-  def accept(visitor : Visitor)
-    visitor.visit(self)
+  def right
+    @right
   end
 end
+
+# class Variable < Expression
+#   def initialize(@name : Token)
+#   end
+
+#   def accept(visitor : Visitor)
+#     visitor.visit(self)
+#   end
+# end
