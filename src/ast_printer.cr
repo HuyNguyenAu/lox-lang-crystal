d8c173073003696ff95fcd2bd8e7950c531058f0
HuyNguyenAu <huynguyenau@gmail.com>
2021-11-02 21:29:38 +1100

    Commented out printer.

--- src/ast_printer.cr
+++ src/ast_printer.cr
@@ -1,49 +1,49 @@
-require "../src/expression.cr"
-require "../src/token.cr"
-require "../src/token-type.cr"
+# require "../src/expression.cr"
+# require "../src/token.cr"
+# require "../src/token-type.cr"
 
-class ASTPrinter < Visitor
-  def print(expression : Expression) : String
-    expression.accept(self)
-  end
+# class ASTPrinter < Visitor
+#   def print(expression : Expression) : String
+#     expression.accept(self)
+#   end
 
-  def visit(expression : Binary) : String
-    parenthesise(expression.operator.lexeme, [expression.left, expression.right])
-  end
+#   def visit(expression : Binary) : String
+#     parenthesise(expression.operator.lexeme, [expression.left, expression.right])
+#   end
 
-  def visit(expression : Grouping) : String
-    parenthesise("group", [expression.expression])
-  end
+#   def visit(expression : Grouping) : String
+#     parenthesise("group", [expression.expression])
+#   end
 
-  def visit(expression : Literal) : String
-    # We need to conform to Java's null as a string behaviour.
-    if expression.value.nil?
-      return "null"
-    end
+#   def visit(expression : Literal) : String
+#     # We need to conform to Java's null as a string behaviour.
+#     if expression.value.nil?
+#       return "null"
+#     end
 
-    "#{expression.value}"
-  end
+#     "#{expression.value}"
+#   end
 
-  def visit(expression : Unary) : String
-    parenthesise(expression.operator.lexeme, [expression.right])
-  end
+#   def visit(expression : Unary) : String
+#     parenthesise(expression.operator.lexeme, [expression.right])
+#   end
 
-  # Convert an expression into it's syntax tree text representation.
-  def parenthesise(name : String, expressions : Array(Expression)) : String
-    builder = "(#{name}"
+#   # Convert an expression into it's syntax tree text representation.
+#   def parenthesise(name : String, expressions : Array(Expression)) : String
+#     builder = "(#{name}"
 
-    i = 0
-    while true
-      if i > expressions.size - 1
-        break
-      end
-      expression = expressions[i]
-      builder += " #{expression.accept(self)}"
-      i += 1
-    end
+#     i = 0
+#     while true
+#       if i > expressions.size - 1
+#         break
+#       end
+#       expression = expressions[i]
+#       builder += " #{expression.accept(self)}"
+#       i += 1
+#     end
 
-    builder += ")"
+#     builder += ")"
 
-    builder
-  end
-end
+#     builder
+#   end
+# end
